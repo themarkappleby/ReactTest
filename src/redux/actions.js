c8f97e2baa7b22cd9fea8c1d960bd9807f78a74a
themarkappleby <themarkappleby@gmail.com>
2016-03-17 11:09:13 -0400

    connected to standalone local api

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,22 @@
 import fetch from 'isomorphic-fetch';
 import {createAction} from 'redux-actions';
 
+const API_ENDPOINT = 'http://localhost:3001/api/movies';
+
 export const selectMovie = createAction('SELECT_MOVIE', movie => movie.id);
 
-export const addMovie = createAction('ADD_MOVIE');
+export const addMovie = createAction('ADD_MOVIE', movie => {
+  return fetch(API_ENDPOINT, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(movie)
+  })
+  .then(response => response.json());
+});
 
-export const fetchMovies = createAction('FETCH_MOVIES', dispatch => {
-  return fetch('/assets/movies.json').then(response => response.json());
+export const fetchMovies = createAction('FETCH_MOVIES', () => {
+  return fetch(API_ENDPOINT).then(response => response.json());
 });
--- src/redux/reducers/movies.js
+++ src/redux/reducers/movies.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
   switch (action.type) {
   case 'ADD_MOVIE':
-    return [...state, action.payload];
+    return action.payload;
   case 'FETCH_MOVIES':
     return action.payload;
   default:
